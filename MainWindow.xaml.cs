682c0f4298c5eaf6e45d8043254a542d253c4f57
ȘTEFANIA SOREA <stefania.sorea@stud.ubbcluj.ro>
2020-10-21 13:08:10 +0300

    lab3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,76 @@ namespace Sorea_Stefania_Lab2
         {
             InitializeComponent();
         }
+        private DoughnutMachine myDoughnutMachine;
+
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            myDoughnutMachine = new DoughnutMachine();
+            myDoughnutMachine.DoughnutComplete += new DoughnutMachine.DoughnutCompleteDelegate(DoughnutCompleteHandler);
+        }
+        private int mRaisedGlazed;
+        private int mRaisedSugar;
+        private int mFilledLemon;
+        private int mFilledChocolate;
+        private int mFilledVanilla;
+
+        private void exitToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void glazedMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedMenuItem.IsChecked = true;
+            sugarMenuItem.IsChecked = false;
+            myDoughnutMachine.MakeDoughnuts(DoughnutType.Glazed);
+        }
+
+        private void sugarMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            sugarMenuItem.IsChecked = true;
+            glazedMenuItem.IsChecked = false;
+            myDoughnutMachine.MakeDoughnuts(DoughnutType.Sugar);
+        }
+
+        private void mnuStop_Click(object sender, RoutedEventArgs e)
+        {
+            myDoughnutMachine.Enabled = false;
+        }
+
+        private void DoughnutCompleteHandler()
+        {
+            switch (myDoughnutMachine.Flavor)
+            {
+                case DoughnutType.Glazed:
+                    mRaisedGlazed++;
+                    txtRaisedGlazed.Text = mRaisedGlazed.ToString();
+                    break;
+                case DoughnutType.Sugar:
+                    mRaisedSugar++;
+                    txtSugarRaised.Text = mRaisedSugar.ToString();
+                    break;
+                case DoughnutType.Lemon:
+                    mFilledLemon++;
+                    txtLemonFilled.Text = mFilledLemon.ToString();
+                    break;
+                case DoughnutType.Chocolate:
+                    mFilledChocolate++;
+                    txtChocolateFilled.Text = mFilledChocolate.ToString();
+                    break;
+                case DoughnutType.Vanilla:
+                    mFilledVanilla++;
+                    txtVanillaFilled.Text = mFilledVanilla.ToString();
+                    break;
+            }
+        }
+
+        private void txtQuantity_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(!(e.Key>=Key.D0 && e.Key<=Key.D9))
+            {
+                MessageBox.Show("Numai cifre se pot introduce!", "InputError", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
